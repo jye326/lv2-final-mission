19c76e441d7ae385e75140ef2c98588ebce10c5d
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-06-10 17:54:40 +0900

    feat: JPA 엔티티 추가 및 의존성 업데이트
    - `Member`, `Reservation` JPA 엔티티 클래스 추가
    - `spring-boot-starter-data-jpa` 의존성 추가
    - JWT 라이브러리 버전 업데이트 및 롬복 의존성 추가

---
+++ src/main/java/finalmission/business/model/entity/Reservation.java
@@ -0,0 +1,45 @@
+package finalmission.business.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private Member member;
+    @Setter
+    private String passportId;  // 여권번호
+
+    private LocalDateTime departureDateTime;
+    private LocalDateTime arrivalDateTime;
+
+    private String departures;  // 출발지
+    private String arrivals;    // 도착지
+
+    private String flightCode;  // 항공기 편명
+
+    public Reservation() {
+    }
+
+    public static Reservation create(Member member, String passportId, LocalDateTime departureDateTime,
+                                     LocalDateTime arrivalDateTime, String departures, String arrivals,
+                                     String flightCode) {
+        return new Reservation(null, member, passportId, departureDateTime, arrivalDateTime, departures, arrivals,
+                flightCode);
+    }
+
+}
